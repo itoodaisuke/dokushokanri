87466525176772b4a3ee6ab6b1e5d4cfc3ffff6b
itoodaisuke <itoodaisuke@gmail.com>
2013-11-02 17:56:24 +0900

    Add bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031122007) do
+ActiveRecord::Schema.define(version: 20131102065441) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20131031122007) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "published_on"
+    t.integer  "isbn"
   end
 
 end
