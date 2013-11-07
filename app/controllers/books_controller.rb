2bcd857746c44583ab869077615213756dba673f
itoodaisuke <itoodaisuke@gmail.com>
2013-11-07 10:34:38 +0900

    add permit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -78,6 +78,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :isbn, :publishers)
+      params.require(:book).permit(:title, :isbn, publisher_attributes: [ :name ])
     end
 end
