4e6b02bbd1d7a26805383c255d53a3b4b7e7b9eb
itoodaisuke <itoodaisuke@gmail.com>
2014-01-15 20:14:07 +0900

    add devise.ja.yml

--- db/migrate/20140115100606_add_omni_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddOmniToUser < ActiveRecord::Migration
-  def change
-    ##Omniauthable
-    add_column :users, :uid, :integer, :limit => 8
-    add_column :users, :name, :string
-    add_column :users, :provider, :string
-  end
-end
